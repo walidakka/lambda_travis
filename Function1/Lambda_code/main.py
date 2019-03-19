7b9bee67dff7528cf0c74735b6e40fad38774153
Walid Khlouge <walid.khlouge@akka.eu>
2019-03-20 00:04:39 +0100

    trigger change test

--- Function1/Lambda_code/main.py
+++ Function1/Lambda_code/main.py
@@ -3,7 +3,7 @@ import logging, json, boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-#
+
 def handler(event,context):
     filename   = event['Records'][0]['s3']['object']['key']
     bucketname = event['Records'][0]['s3']['bucket']['name']
