4d72732aa60ac115a2a967e720d9aa176ebc95b4
Walid Khlouge <walid.khlouge@akka.eu>
2019-03-20 16:36:44 +0100

    fix test object key

--- Lambda_code/tests/test_lambda_E2E.py
+++ Lambda_code/tests/test_lambda_E2E.py
@@ -13,7 +13,7 @@ def test_lambda_E2E():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(str(output['bucket_name']['value']))
     #bucket.put_object(Key="something.ezazekssea.zeaaze.sss",Body=b'foobar')
-    bucket.upload_file(THIS_FOLDER+'test_files/1280px-Git-logo.svg.png')
+    bucket.upload_file(THIS_FOLDER+'test_files/1280px-Git-logo.svg.png','1280px-Git-logo.svg.png')
     time.sleep(3)
     assert len(list(bucket.objects.filter(Prefix="1280px-Git-logo.svg.jpg"))) == 1
     assert len(list(bucket.objects.filter(Prefix="1280px-Git-logo.svg.png"))) == 0
