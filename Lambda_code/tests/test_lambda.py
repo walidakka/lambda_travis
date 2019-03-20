fbc1141e18cc5937b60a68adbc389d23b9cc9238
Walid Khlouge <walid.khlouge@akka.eu>
2019-03-20 11:32:12 +0100

    edit gitignore & test

--- Lambda_code/tests/test_lambda.py
+++ Lambda_code/tests/test_lambda.py
@@ -1,16 +1,15 @@
-import boto3
+import boto3, pytest, os, json
 from .. import main
 from moto import mock_s3
-import pytest
 
-
-with open('tests/event.json') as f:
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(THIS_FOLDER, 'event.json')) as f:
     event = json.load(f)
 @mock_s3
-def test_handler():
+def test_handler_rename():
     conn = boto3.resource('s3', region_name='eu-west-1')
     bucket = conn.create_bucket(Bucket=event['Records'][0]['s3']['bucket']['name'])
     bucket.put_object(Key=event['Records'][0]['s3']['object']['key'])
     result = main.handler(event,None)
-    assert len(list(bucket.objects.filter(Prefix=result['body']['Newfile']))) == 1
-    assert len(list(bucket.objects.filter(Prefix=result['body']['Oldfile']))) == 0
+    assert len(list(bucket.objects.filter(Prefix=result['Newfile']))) == 1
+    assert len(list(bucket.objects.filter(Prefix=result['Oldfile']))) == 0
