efdfb7561e7653630a11ff1b7875a899d4e4c76b
Walid Khlouge <walid.khlouge@akka.eu>
2019-03-20 16:32:15 +0100

    fix test

--- Lambda_code/tests/test_lambda_E2E.py
+++ Lambda_code/tests/test_lambda_E2E.py
@@ -13,7 +13,7 @@ def test_lambda_E2E():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(str(output['bucket_name']['value']))
     #bucket.put_object(Key="something.ezazekssea.zeaaze.sss",Body=b'foobar')
-    for filename in os.listdir(THIS_FOLDER+'/test_files'):
-        bucket.upload_file(os.path.join(THIS_FOLDER+'/test_files',filename),filename)
-        time.sleep(1)
-        assert len(list(bucket.objects.filter(Prefix=filename[:-len(filename.split('.')[-1])] + 'jpg'))) == 1
+    bucket.upload_file(THIS_FOLDER+'test_files/1280px-Git-logo.svg.png')
+    time.sleep(3)
+    assert len(list(bucket.objects.filter(Prefix="1280px-Git-logo.svg.jpg"))) == 1
+    assert len(list(bucket.objects.filter(Prefix="1280px-Git-logo.svg.png"))) == 0
