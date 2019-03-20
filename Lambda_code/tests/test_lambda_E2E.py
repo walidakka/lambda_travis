b007033d9ac4878f0ca4bf2592e2c0806f2a00fa
Walid Khlouge <walid.khlouge@akka.eu>
2019-03-20 15:10:12 +0100

    fix test

--- Lambda_code/tests/test_lambda_E2E.py
+++ Lambda_code/tests/test_lambda_E2E.py
@@ -11,7 +11,7 @@ with open(os.path.join(THIS_FOLDER, 'output.json')) as f:
 def test_lambda_E2E():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(output['bucket_name']['value'])
-    object = s3.Object(output['bucket_name'],"something.ezazekssea.zeaaze.sss")
+    bucket.put_object("something.ezazekssea.zeaaze.sss")
     time.sleep(3)
     assert len(list(bucket.objects.filter(Prefix="something.ezazekssea.zeaaze.jpg"))) == 1
     assert len(list(bucket.objects.filter(Prefix="something.ezazekssea.zeaaze.sss"))) == 0
